1d224b9b0e9df8eb4c822b7d0a2e001f394c0713
8Eithel8 <eithel@list.ru>
2022-02-17 17:11:02 +0300

    fix: исправить попап

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 let buttonEdit = document.querySelector('.profile__button_type_edit');
 let popup = document.querySelector('.popup');
+// let buttonClose = document.querySelector('');
 
 buttonEdit.addEventListener('click', function() {
 
-  if (popup.classList.contains('opned')) {
-    popup.classList.remove('opened');
-    } else {
-      popup.classList.add('opened');
-  };
-});
+  // if (popup.classList.contains('popup__opened')) {
+  //   popup.classList.remove('popup__opened');
+  //   } else {
+      popup.classList.add('popup__opened');
+  });
+// });
+
+
+// let buttonEdit = document.querySelector('.popup__button');
+// let popup = document.querySelector('.popup');
+
+
+// buttonEdit.addEventListener('click', function() {
+
+//   if (popup.classList.contains('popup__opened')) {
+//     popup.classList.remove('popup__opened');
+//     } else {
+//       popup.classList.add('popup__opened');
+//   };
+// });
