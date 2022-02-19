d23ace9b250ef769263f12c215710b5b6aef5ae5
8Eithel8 <eithel@list.ru>
2022-02-19 13:07:11 +0300

    refactor: исправить по БЭМ, js, удалить шрифты 900

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 let buttonEdit = document.querySelector('.profile__button_type_edit');
 let popup = document.querySelector('.popup');
 let buttonClose = document.querySelector('.popup__button_type_close');
-
 let fieldName = document.querySelector('#name');
 let fieldInfo = document.querySelector('#info');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let buttonSubmit = document.querySelector('.popup__button_type_submit');
 let form = document.querySelector('.popup__form');
 
 function editor() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   fieldName.value = profileTitle.textContent;
   fieldInfo.value = profileSubtitle.textContent;
 };
 
-buttonEdit.addEventListener('click', editor);
-
-function closer () {
-  popup.classList.remove('popup__opened');
+function closer() {
+  popup.classList.remove('popup_opened');
 };
 
-buttonClose.addEventListener('click', closer);
-
-function saver (evt) {
+function saver(evt) {
   evt.preventDefault();
   profileTitle.textContent = fieldName.value;
   profileSubtitle.textContent = fieldInfo.value;
-  popup.classList.remove('popup__opened');
+  closer();
 };
 
+buttonEdit.addEventListener('click', editor);
+buttonClose.addEventListener('click', closer);
 form.addEventListener('submit', saver);
-
-
-
- 
