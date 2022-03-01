ca02b1b09aa672e5e811c9ceab21d9d69d37eb2d
8Eithel8 <eithel@list.ru>
2022-03-01 21:08:10 +0300

    feat: сделать разметку popup-adder

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-let buttonEdit = document.querySelector('.profile__button_type_edit');
-let popup = document.querySelector('.popup');
-let buttonClose = document.querySelector('.popup__button_type_close');
-let fieldName = document.querySelector('#name');
-let fieldInfo = document.querySelector('#info');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let form = document.querySelector('.popup__form');
-
-function editor() {
-  popup.classList.add('popup_opened');
-  fieldName.value = profileTitle.textContent;
-  fieldInfo.value = profileSubtitle.textContent;
-};
-
-function closer() {
-  popup.classList.remove('popup_opened');
-};
-
-function saver(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = fieldName.value;
-  profileSubtitle.textContent = fieldInfo.value;
-  closer();
-};
-
-buttonEdit.addEventListener('click', editor);
-buttonClose.addEventListener('click', closer);
-form.addEventListener('submit', saver);
-
 const initialCards = [
   {
    name: 'Петропавловская крепость',
@@ -54,3 +24,44 @@ const initialCards = [
    link: './images/Mos_obl.jpg'
  },
 ];
+
+let buttonEdit = document.querySelector('.profile__button_type_edit');
+let popup = document.querySelector('.popup');
+let buttonClose = document.querySelector('.popup__button_type_close');
+let fieldName = document.querySelector('#name');
+let fieldInfo = document.querySelector('#info');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let form = document.querySelector('.popup__form');
+let buttonAdd = document.querySelector('.profile__button_type_add');
+let popup = document.querySelector('.popup_type_adder');
+
+function editor() {
+  popup.classList.add('popup_opened');
+  fieldName.value = profileTitle.textContent;
+  fieldInfo.value = profileSubtitle.textContent;
+};
+
+function closer() {
+  popup.classList.remove('popup_opened');
+};
+
+function saver(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = fieldName.value;
+  profileSubtitle.textContent = fieldInfo.value;
+  closer();
+};
+
+function adder(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = fieldName.value;
+  profileSubtitle.textContent = fieldInfo.value;
+  closer();
+};
+
+
+buttonEdit.addEventListener('click', editor);
+buttonClose.addEventListener('click', closer);
+form.addEventListener('submit', saver);
+
