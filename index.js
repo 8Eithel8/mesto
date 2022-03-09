e067351226ed2fcc48ba994640a83705f2e6281a
8Eithel8 <eithel@list.ru>
2022-03-09 21:14:19 +0300

    fix: сделать добавление фоток

--- index.js
+++ index.js
@@ -37,21 +37,17 @@ const fieldInfo = document.querySelector('#info');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__form');
-const cardRemove = document.querySelector('.card__remove');
-const cardsRemove = document.querySelectorAll('.card__remove');
-
-const cardLike = document.querySelector('.card__like');
+// const cardRemove = document.querySelector('.card__remove');
+// const cardsRemove = document.querySelectorAll('.card__remove');
 const cards = document.querySelector('.cards');
-// const newCardAdd = document.querySelector('.popup_type_add');
 const cardAdded = document.querySelector('.added');  /*он нужен для работы лаков. но что-то не понятно*/
 const popupPhoto = document.querySelector('.popup_type_photo');
-const cardImage = document.querySelector('.card__image');
-const newCardForm = document.querySelector('#new-card-form');
+// const cardImage = document.querySelector('.card__image');
+// const newCardForm = document.querySelector('#new-card-form');
 const cardTemplate = document.querySelector('#card-template').content;
 const title = cardTemplate.querySelector('.card__title');
 const image = cardTemplate.querySelector('.card__image');
 const inputPhotoName = document.querySelector('#photo-name');
-
 const inputPhotoLink = document.querySelector('#photo-link');
 
 function addCard(linkValue, titleValue) {
@@ -59,15 +55,23 @@ function addCard(linkValue, titleValue) {
 
   cardItem.querySelector('.card__image').src = linkValue;
   cardItem.querySelector('.card__title').textContent = titleValue;
-
+  cardItem.querySelector('.card__like').addEventListener('click', cardLikeHandler);
+  cardItem.querySelector('.card__remove').addEventListener('click', removeCardHandler);
+  cardItem.querySelector('.card__image').addEventListener('click', photoOpenHandler);
   cards.append(cardItem);
-}
+};
 
 for (let i = 0; i < initialCards.length; i++) {
   addCard(initialCards[i].link, initialCards[i].name)
-  
 };
 
+function cardLikeHandler(evt) {
+  evt.target.classList.toggle('added');
+};
+
+function removeCardHandler(evt) {
+  evt.target.parentNode.remove();
+};
 
 function profileEditHandler() {
   popup.classList.add('popup_opened');
@@ -79,16 +83,11 @@ function formAddHandler() {
   popupAdd.classList.add('popup_opened');
 };
 
-
 function formCloseHandler() {
   for (let i = 0; i < popups.length; i++) {
     popups[i].classList.remove('popup_opened')
   };
 };
-
-// function formCloseHandler(evt) {
-//   evt.target.popup.classList.remove('popup_opened');
-// };
 
 function formCloseHandler() {
   for (let i = 0; i < popups.length; i++) {
@@ -103,71 +102,30 @@ function profileSaveHandler(evt) {
   formCloseHandler();
 };
 
-
-function cardLikeHandler(evt) {
-  evt.target.classList.toggle('added');
-};
-
-function photoOpenHandler() {
+function photoOpenHandler(evt) {
   popupPhoto.classList.add('popup_opened');
-  document.querySelector('.popup__photo').src = image.src;
-  document.querySelector('.popup__photo-title').textContent = title.textContent;
+  document.querySelector('.popup__photo').src = evt.target.src;
+  document.querySelector('.popup__photo-title').textContent = evt.target.parentNode.querySelector('.card__title').textContent;
 };
-
-/*deleteButton.addEventListener('click', function () {
-  const listItem = deleteButton.closest('.todo__item');
-  listItem.remove();
-});  такой вариант предлагают в яндексе*/
-
-function removeCardHandler(evt) {
-  evt.target.parentNode.remove(cardRemove);
-}
-
-function removeCardHandler() {
-  this.parentNode.remove(cardRemove);
-}
 
 for (let i=0; i < buttonsClose.length; i++) {
   buttonsClose[i].addEventListener('click', formCloseHandler);
 };
 
-
-for (let i=0; i < cardsRemove.length; i++) {
-  cardsRemove[i].addEventListener('click', removeCardHandler);
-};
-
 buttonEdit.addEventListener('click', profileEditHandler);
 buttonAdd.addEventListener('click', formAddHandler);
 form.addEventListener('submit', profileSaveHandler);
-cardImage.addEventListener('click', photoOpenHandler);
-cardRemove.addEventListener('click', removeCardHandler);
-cardLike.addEventListener('click', cardLikeHandler);
-
-
-// function addCard(linkValue, titleValue) {
-//   const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-//   cardItem.querySelector('.card__image').src = linkValue;
-//   cardItem.querySelector('.card__title').textContent = titleValue;
-  
-
-//   console.log(cards);
-//   cards.append(cardItem);
-// }
 
 const saveButton = document.querySelector('#button-save');
 
 saveButton.addEventListener('click', function () {
   
-    let image = inputPhotoLink;
-    let title = inputPhotoName; 
-    console.log(inputPhotoName);
+  let image = inputPhotoLink;
+  let title = inputPhotoName; 
+  console.log(inputPhotoName);
   addCard(image.value, title.value);
   title.value = '';
   image.value = '';
   formCloseHandler();
 }); 
 
-// for (let i = 0; i < initialCards.length; i++) {
-//   addCard(initialCards[i].link, initialCards[i].name)
-  
-// };
