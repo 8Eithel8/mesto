5d9d4f0e2f7897c1714d57f90cbb98ce9e392693
8Eithel8 <eithel@list.ru>
2022-05-06 13:07:48 +0300

    feat: add like-counter

---
+++ src/components/Api.js
@@ -0,0 +1,21 @@
+// TODO: Для работы с API создайте класс Api. Все запросы должны быть методами этого класса:
+
+class Api {
+    constructor(options) {
+        // тело конструктора
+    }
+
+    getInitialCards() {
+        // ...
+    }
+
+    // другие методы работы с API
+}
+
+const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+    headers: {
+        authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+        'Content-Type': 'application/json'
+    }
+});
