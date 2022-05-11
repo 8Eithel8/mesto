3c375cd62e661f5d31a417582e81e3fc23a9fea0
8Eithel8 <eithel@list.ru>
2022-05-11 20:55:28 +0300

    fix: class api

--- src/components/Api.js
+++ src/components/Api.js
@@ -14,7 +14,7 @@ class Api {
         }
     }
 
-
+    //загружаем фото с сервера
     getInitialCards() {
         return fetch(`${this._baseUrl}/cards`, {
             method: 'GET',
@@ -23,6 +23,19 @@ class Api {
             .then(this._checkRes)
     }
 
+    //добавляем аватар
+    addAvatar(avatar) {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                avatar
+            })
+        })
+            .then(this._checkRes)
+    }
+
+    //получаем инфу о пользователе с сервера
     getUserInfo() {
         return fetch(`${this._baseUrl}/users/me`, {
             method: 'GET',
@@ -31,6 +44,7 @@ class Api {
             .then(this._checkRes)
     }
 
+    //изменяем инфу о пользователе
     editUserInfo() {
         return fetch(`${this._baseUrl}/users/me`, {
             method: 'PATCH',
@@ -39,47 +53,44 @@ class Api {
             .then(this._checkRes)
     }
 
-
-    editUserInfo() {
-        return fetch('https://nomoreparties.co/v1/cohort-41/users/me', {
-            method: 'PATCH',
-            headers: {
-                authorization: '25211fd8-3e01-4ad9-a1d8-b38f3a1a11d7',
-                'Content-Type': 'application/json'
-            },
+    //постим новую фотографию
+    postNewCard(name, link) {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: 'POST',
+            headers: this._headers,
             body: JSON.stringify({
-                name: 'Marie Skłodowska Curie',
-                about: 'Physicist and Chemist'
+                name,
+                link
             })
         })
-            // .then(res => {
-            //     if (res.ok) {
-            //         return res.json();
-            //     }
-            //
-            //     // если ошибка, отклоняем промис
-            //     return Promise.reject(`Ошибка: ${res.status}`);
-            // });
+            .then(this._checkRes)
     }
-
-    postNewCard() {
-        return fetch('https://mesto.nomoreparties.co/v1/cohort-41/cards', {
-            method: 'POST',
-            headers: {
-                authorization: '25211fd8-3e01-4ad9-a1d8-b38f3a1a11d7'
-            }
+    //удалаяем фото
+    removeCard(id) {
+        return fetch(`${this._baseUrl}/cards/${id}`, {
+            method: 'DELETE',
+            headers: this._headers
         })
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-
-                // если ошибка, отклоняем промис
-                return Promise.reject(`Ошибка: ${res.status}`);
-            });
+            .then(this._checkRes)
+    }
+    //ставим лайк
+    addLike(id) {
+        return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+            method: 'PUT',
+            headers: this._headers
+        })
+            .then(this._checkRes)
+    }
+    //убираем лайк
+    removeLike(id) {
+        return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+            method: 'DELETE',
+            headers: this._headers
+        })
+            .then(this._checkRes)
     }
 }
-}
+
 export const  api = new Api({
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-41',
     headers: {
